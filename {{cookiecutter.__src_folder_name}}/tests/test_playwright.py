911bf6ca37211325c909c6b22ee5ae479c6c803d
Jay Miller <kjaymiller@gmail.com>
2023-07-12 16:14:20 -0700

    adds page tests

--- {{cookiecutter.__src_folder_name}}/tests/test_playwright.py
+++ {{cookiecutter.__src_folder_name}}/tests/test_playwright.py
@@ -1,69 +1,70 @@
 # from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import re
 import os
 import pytest
 from playwright.sync_api import expect, Page, expect
 
 
-    # def setUpClass(cls):
-    #     os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-    #     super().setUpClass()
-    #     cls.playwright = sync_playwright().start()
-    #     cls.browser = cls.playwright.chromium.launch()
-
-    # @classmethod
-    # def tearDownClass(cls):
-    #     super().tearDownClass()
-    #     cls.browser.close()
-    #     cls.playwright.stop()
-
 @pytest.fixture(scope="session")
 def mock_functions_env():
     os.environ['DJANGO_ALLOW_ASYNC_UNSAFE'] = "true"
-    
-def test_request_info(page: Page, live_server):
+
+def expect_header_url_test(page:Page, url:str, name: str, re_str: str) -> None:
+    """Helper function to test that a link has a certain attribute"""
+    page.goto(url)
+    header = page.locator("nav")
+
+    # Request Info
+    request_info = header.get_by_role("link", name=name)
+    expect(request_info).to_have_attribute("href", re.compile(re_str))
+
+
+def test_home(mock_functions_env, page: Page, live_server):
+    """Test that the home page loads"""
+    page.goto(live_server.url)
+    expect(page).to_have_title("ReleCloud - Expand your horizons")
+    page.close()
+
+
+def test_header_has_request_info(mock_functions_env, page: Page, live_server):
+    """Test that the header loads with links"""
+
+    header_links = (
+        ("Request Information", ".*info.*"),
+        ("Destinations", ".*destination.*"),
+        ("About", ".*about.*"),
+    )
+
+    for title, re_str in header_links:
+        expect_header_url_test(
+            page,
+            url=live_server.url,
+            name=title,
+            re_str=re_str,
+        )
+    page.close()
+
+
+def test_request_information(mock_functions_env, page: Page, live_server):
+    """Test that the request info form page loads"""
     page.goto(live_server.url)
     page.get_by_role("link", name="Request Information").click()
     expect(page).to_have_title("ReleCloud - Request information")
-    # page.get_by_label("Name*").click()
-    # page.get_by_label("Name*").fill("pamela")
-    # page.get_by_label("Name*").press("Tab")
-    # page.get_by_label("Email*").fill("pammyla.fox@gmail.com")
-    # page.get_by_label("Email*").press("Tab")
-    # page.get_by_role("combobox", name="Cruise*").select_option("The Sun Tour")
-    # page.get_by_label("Notes*").click()
-    # page.get_by_label("Notes*").fill("more mars rovers pls")
-    # page.get_by_role("button", name="Save").click()
-    # page.get_by_role("alert").click()
-    # expect(page.locator("[role=alert]")).to_contain_text(
-    #     "Thank you, pamela! We will email you when we have more information about The Sun"
-    # )
     page.close()
 
-    # def test_destinations(self):
-    #     page = self.browser.new_page()
-    #     page.goto(self.live_server_url)
-    #     page.get_by_role("link", name="Destinations").click()
-    #     expect(page.get_by_role("heading", name="Cruise destinations")).to_be_visible()
-    #     page.get_by_role("link", name="Mars").click()
-    #     expect(page.get_by_role("heading", name="Mars")).to_be_visible()
-    #     expect(page.get_by_text("It's red")).to_be_visible()
-    #     page.get_by_role("link", name="The Hot Tour").click()
-    #     expect(page.get_by_role("heading", name="The Hot Tour")).to_be_visible()
-    #     page.get_by_role("link", name="Mercury").click()
-    #     expect(page.get_by_role("heading", name="Mercury")).to_be_visible()
 
-    # def test_about(self):
-    #     page = self.browser.new_page()
-    #     page.goto(self.live_server_url)
-    #     page.get_by_role("link", name="About").click()
-    #     expect(page.get_by_role("heading", name="About ReleCloud")).to_be_visible()
+def test_destinations_(mock_functions_env, page: Page, live_server):
+    """Test that the request info form page loads"""
+    page.goto(live_server.url)
+    page.get_by_role("link", name="Destinations").click()
+    expect(page).to_have_title("ReleCloud - Destinations")
+    page.close()
 
-    # def test_admin_login_not_allowed(self):
-    #     page = self.browser.new_page()
-    #     page.goto(f"{self.live_server_url}/admin/")
-    #     page.wait_for_selector("text=Django administration")
-    #     page.fill("[name=username]", "myuser")
-    #     page.fill("[name=password]", "secret")
-    #     page.click("text=Log in")
-    #     assert len(page.eval_on_selector(".errornote", "el => el.innerText")) > 0
-    #     page.close()
+
+def test_about(mock_functions_env, page: Page, live_server):
+    """Test that the request info form page loads"""
+    page.goto(live_server.url)
+    page.get_by_role("link", name="About").click()
+    expect(page).to_have_title("ReleCloud - About")
+    expect(page).to_have_text("About ReleCloud")
+    page.close()
