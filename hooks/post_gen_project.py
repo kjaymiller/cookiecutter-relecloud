8ae493c8dd74d9fcf2d1078c192c7a41b27449b4
Pamela Fox <pamela.fox@gmail.com>
2023-08-03 12:45:13 -0700

    Just make it overwrite

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import pathlib
 import shutil
 import sys
@@ -10,11 +11,8 @@ import sys
 def move_root_files():
     try:
         root_folder = pathlib.Path("root")
-
-        for file in root_folder.iterdir():
-            shutil.move(file, pathlib.Path.cwd().parent)
-        
-        root_folder.rmdir()
+        shutil.copytree(root_folder, pathlib.Path.cwd().parent, dirs_exist_ok=True)
+        shutil.rmtree(root_folder)
 
     except Exception as e:
         # exit with status 1 to indicate failure
@@ -35,11 +33,8 @@ def rename_backend_files():
     for unused_backend in project_backends:
         shutil.rmtree(pathlib.Path(unused_backend))
         
-    # rename the selected backend folder to backend
-    for file in pathlib.Path(selected_backend).iterdir():
-            shutil.move(file, pathlib.Path.cwd())
-
-    pathlib.Path(selected_backend).rmdir()
+    shutil.copytree(pathlib.Path(selected_backend), pathlib.Path.cwd(), dirs_exist_ok=True)
+    shutil.rmtree(pathlib.Path(selected_backend))
 
 move_root_files()
 rename_backend_files()
