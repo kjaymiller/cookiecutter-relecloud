aa5adc4a861fa1422931e6c52c06fc32bc6694a8
Pamela Fox <pamela.fox@gmail.com>
2023-08-17 15:48:14 -0700

    Fix tests

--- {{cookiecutter.__src_folder_name}}/src/tests/conftest.py
+++ {{cookiecutter.__src_folder_name}}/src/tests/conftest.py
@@ -80,23 +80,20 @@ def app():
         engines[key] = engine
 {% endif %}
 {% if cookiecutter.project_backend == "django" %}
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def django_db_setup(django_db_setup, django_db_blocker):
+    """ Overrides the default django_db_setup fixture to load the seed data
+    https://pytest-django.readthedocs.io/en/latest/database.html#populate-the-test-database-if-you-use-transactional-or-live-server
+    """
     with django_db_blocker.unblock():
         call_command("loaddata", "src/seed_data.json")
-{% endif %}
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def mock_functions_env():
-    {% if cookiecutter.project_backend == "django" %}
     os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-    {% else %}
-    pass
-    {% endif %}
 
 
-{% if cookiecutter.project_backend == "django" %}
 @pytest.fixture(scope="session")
 def live_server_url(live_server):
     """Returns the url of the live server"""
