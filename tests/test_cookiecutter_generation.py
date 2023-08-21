d10361c9f08030c84923a115c1f22da79c3c47d9
Jay Miller <kjaymiller@gmail.com>
2023-08-21 14:25:42 -0400

    moves confest content

--- tests/test_cookiecutter_generation.py
+++ tests/test_cookiecutter_generation.py
@@ -4,36 +4,8 @@ This DOES NOT TEST AZURE DEPLOYMENT
 """
 import re
 import pytest
-import itertools
 import pathlib
 import subprocess
-
-
-web_frameworks = ["django", "flask", "fastapi"]
-db_resources = ["postgres-flexible"]
-
-combinations = itertools.product(web_frameworks, db_resources)
-# Creates the context override for the parametrized test
-CONTEXT_OVERRIDE = [{"project_backend": x, "db_resource": y} for x, y in combinations]
-
-
-@pytest.fixture(scope="session")
-def context():
-    return {
-        "project_name": "Long_MIXED_CASE-demo name",
-        "azd_template_version": "0.0.1",
-        "project_backend": ["django", "fastapi", "flask"],
-        "use_vnet": "n",
-        "db_resource": ["postgres-flexible",  "cosmos-postgres",],
-        "web_port": "8000",
-    }
-
-
-@pytest.fixture(scope="module", params=[*CONTEXT_OVERRIDE])
-def bakery(request, context, cookies_session):
-    extra_context = {**context, **request.param}
-    result = cookies_session.bake(extra_context=extra_context)
-    yield result
 
 
 def test_project_generation(bakery):
@@ -77,15 +49,15 @@ def test_all_cookiecutter_paths_generated(bakery):
                 pytest.fail(f"Found cookiecutter variable in {rel_path} - {matches}")
 
 
-def test_build_folders_are_deleted(bakery, context):
-    backend_names = context.get("project_backend")
+def test_build_folders_are_deleted(bakery, default_context):
+    backend_names = default_context.get("project_backend")
     for path in bakery.project_path.iterdir():
         if path.is_dir():
             if path.name in backend_names:
                 pytest.fail(f"Found build folder {path.name}")
 
 
-def tests_valid_bicep(bakery, context):
+def tests_valid_bicep(bakery):
     commands = (
         f"az bicep build --file {bakery.project_path}/infra/main.bicep".split(
             " "
@@ -93,3 +65,9 @@ def tests_valid_bicep(bakery, context):
     )
     result = subprocess.run(commands, capture_output=True, text=True)
     assert result.returncode == 0
+
+@pytest.mark.skip(reason="not implmented yet")
+def tests_mongo_builds_use_mongo_db_vars(bakery, default_context):
+    if "mongo" in default_context.get("db_resource"):
+        # read the contents from the generated 
+        pass
