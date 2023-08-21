2c47f8d67d343fd3f2aa29d08c0af8c06c46b84d
Jay Miller <kjaymiller@gmail.com>
2023-08-21 14:28:02 -0400

    adds mongo_env_info

--- {{cookiecutter.__src_folder_name}}/src/flask/flaskapp/config/development.py
+++ {{cookiecutter.__src_folder_name}}/src/flask/flaskapp/config/development.py
@@ -6,10 +6,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True
 
+{% if 'postgres' in cookiecutter.db_resource %}
 dbuser = os.environ["POSTGRES_USERNAME"]
 dbpass = os.environ["POSTGRES_PASSWORD"]
 dbhost = os.environ["POSTGRES_HOST"]
 dbname = os.environ["POSTGRES_DATABASE"]
 DATABASE_URI = f"postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}"
+{% endif %}
+
+{% if 'mongo' in cookiecutter.db_resource %}
+dbuser = os.environ["MONGO_USERNAME"]
+dbpass = os.environ["MONGO_PASSWORD"]
+dbhost = os.environ["MONGO_HOST"]
+dbname = os.environ["MONGO_DATABASE"]
+DATABASE_URI  = f"mongodb://{dbuser}:{dbpass}@{dbhost}/{dbname}"
+{% endif %}
 
 TIME_ZONE = "UTC"
--- {{cookiecutter.__src_folder_name}}/src/flask/flaskapp/config/production.py
+++ {{cookiecutter.__src_folder_name}}/src/flask/flaskapp/config/production.py
@@ -1,5 +1,10 @@
 import os
+{% if 'mongo' in cookiecutter.db_resource %}
+import pymongo
+{% endif %}
 
+# Create a mongoDB Connection
+client = pymongo.MongoClient("DATABASE_CONNECTION_STRING")
 DEBUG = False
 
 if "WEBSITE_HOSTNAME" in os.environ:
@@ -10,8 +15,20 @@ else:
 {% if cookiecutter.db_resource == "postgres-service" %}
 # The PostgreSQL service binding will always set env variables with these names.
 {% endif %}
+
+{% if 'postgres' in cookiecutter.db_resource %}
 dbuser = os.environ["POSTGRES_USERNAME"]
 dbpass = os.environ["POSTGRES_PASSWORD"]
 dbhost = os.environ["POSTGRES_HOST"]
 dbname = os.environ["POSTGRES_DATABASE"]
 DATABASE_URI = f"postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}"
+{% endif %}
+
+{% if 'mongo' in cookiecutter.db_resource %}
+dbuser = os.environ["MONGO_USERNAME"]
+dbpass = os.environ["MONGO_PASSWORD"]
+dbhost = os.environ["MONGO_HOST"]
+dbname = os.environ["MONGO_DATABASE"]
+DATABASE_URI  = f"mongodb://{dbuser}:{dbpass}@{dbhost}/{dbname}"
+{% endif %}
+
