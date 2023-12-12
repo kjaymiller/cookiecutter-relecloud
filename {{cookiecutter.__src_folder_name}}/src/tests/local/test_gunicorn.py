c85d892e7f9f3c7903e6b1cd1e1fe7f7182a454d
Pamela Fox <pamela.fox@gmail.com>
2023-12-12 13:48:04 -0800

    Fix syntax

--- {{cookiecutter.__src_folder_name}}/src/tests/local/test_gunicorn.py
+++ {{cookiecutter.__src_folder_name}}/src/tests/local/test_gunicorn.py
@@ -7,7 +7,7 @@ from gunicorn.app.wsgiapp import run
 
 def test_config_imports():
     {% set app_arg = {"flask": "flaskapp:create_app()", "fastapi": "fastapi_app.app:app", "django": "project.wsgi:application"}[cookiecutter.project_backend] %}
-    argv = ["gunicorn", "--check-config", {{ app_arg }}, "-c", "src/gunicorn.conf.py"]
+    argv = ["gunicorn", "--check-config", "{{ app_arg }}", "-c", "src/gunicorn.conf.py"]
 
     with mock.patch.object(sys, "argv", argv):
         with pytest.raises(SystemExit) as excinfo:
