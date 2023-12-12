1dbf27dbaaa6ba5342dab6171e263927acb415da
Pamela Fox <pamela.fox@gmail.com>
2023-12-12 12:18:56 -0800

    Add Flask client tests and gunicorn tests

--- {{cookiecutter.__src_folder_name}}/src/flask/flaskapp/pages.py
+++ {{cookiecutter.__src_folder_name}}/src/flask/flaskapp/pages.py
@@ -45,13 +45,13 @@ def cruise_detail(pk: int):
     )
 
 
-@bp.get("/info_request/")
+@bp.get("/info_request")
 def info_request():
     {{ get_all("Cruise") }}
     return render_template("info_request_create.html", cruises=all_cruises, message=request.args.get("message"))
 
 
-@bp.post("/info_request/")
+@bp.post("/info_request")
 def create_info_request():
     name = request.form["name"]
     db_info_request = models.InfoRequest(
