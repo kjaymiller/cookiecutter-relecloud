1e30193c176d455581b62760cd7cdeed30096894
Jay Miller <kjaymiller@gmail.com>
2024-01-17 12:26:39 -0500

    adds conditional to init

--- {{cookiecutter.__src_folder_name}}/src/fastapi/fastapi_app/__init__.py
+++ {{cookiecutter.__src_folder_name}}/src/fastapi/fastapi_app/__init__.py
@@ -1,5 +1,7 @@
+import os
 from .app import app  # noqa
 from azure.monitor.opentelemetry import configure_azure_monitor
 
-configure_azure_monitor()
+if os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
+    configure_azure_monitor()
 __all__ = ["app"]
